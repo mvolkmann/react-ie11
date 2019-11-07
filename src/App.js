128d725b46639f1d84117566738fa3d0f751ad5c
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2019-11-07 14:57:04 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="a">A</div>
+      <div className="b">B</div>
+      <div className="c">C</div>
+      <div className="d">D</div>
+    </div>
+  );
+}
+
+export default App;
